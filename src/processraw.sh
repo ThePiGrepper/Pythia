b542351b05fbb3f5e6e645eebaec195c507a0d66
Erick Cafferata <nagato@god.com>
2016-07-27 11:47:51 -0300

    processraw.sh: numeric sorted IDs.

--- src/processraw.sh
+++ src/processraw.sh
@@ -4,15 +4,19 @@ rm -rf ../doc/collecteddata.t
 rm -rf ../data/index
 mkdir -p ../doc
 mkdir -p ../data/index
+
+#create report
 echo "Data Collected" >> ../doc/collecteddata.t
 p=$(cat ../data/raw/* | wc -l)
 echo "Overall Samples: $p">> ../doc/collecteddata.t
 p=$(cat ../data/raw/* | sed -n "/:1$/ p" | wc -l)
-cat ../data/raw/* | sed -n "/:1$/ p" | cut -d':' -f1 >> ../data/index/prepainted.t
 echo "prepainted: $p" >> ../doc/collecteddata.t
 p=$(cat ../data/raw/* | sed -n "/:2$/ p" | wc -l)
-cat ../data/raw/* | sed -n "/:2$/ p" | cut -d':' -f1 >> ../data/index/action.t
 echo "action/dolls: $p" >> ../doc/collecteddata.t
 p=$(cat ../data/raw/* | sed -n "/:$/ p" | wc -l)
-cat ../data/raw/* | sed -n "/:$/ p" | cut -d':' -f1 >> ../data/index/null.t
 echo "null: $p" >> ../doc/collecteddata.t
+
+#dump data
+cat ../data/raw/* | sed -n "/:1$/ p" | cut -d':' -f1 | sort -g >> ../data/index/prepainted.t
+cat ../data/raw/* | sed -n "/:2$/ p" | cut -d':' -f1 | sort -g >> ../data/index/action.t
+cat ../data/raw/* | sed -n "/:$/ p" | cut -d':' -f1 | sort -g >> ../data/index/null.t
